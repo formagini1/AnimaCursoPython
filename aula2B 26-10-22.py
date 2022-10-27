3347f0e4ecb52592986e15eb556c7e54ecf42453
Ryan Augusto <90337265+formagini1@users.noreply.github.com>
2022-10-27 01:16:42 +0000

    s

---
+++ aula2B 26-10-22.py
@@ -0,0 +1,15 @@
+#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
+nome = input("Escreva seu nome: ")
+nota = float(input("Digite sua nota: "))
+
+if (nota == 10):
+  print(f"{nome}, você é o bichão memo...");
+
+elif (nota >=6 and nota<=10 ):
+  print(f"{nome} foi bem, mas nem tanto...")
+  
+else:
+  print("precisa estudar...")
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
-nome = input("Escreva seu nome: ")
-nota = int(input("Digite sua nota: "))
+print("Aqui é da forma rústica...")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
 
+print("Aqui é do jeito certo")
+numero = 1
+print(numero)
+numero = numero + 1
+print(numero)
