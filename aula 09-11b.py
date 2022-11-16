693d0b873fe24928d68b4bd841242889ec7fbd7c
Ryan Augusto <90337265+formagini1@users.noreply.github.com>
2022-11-16 23:32:48 +0000

    aula de banco de dados

--- aula 09-11b.py
+++ aula 09-11b.py
@@ -12,13 +12,23 @@ print(imposto2)
 
 #criar uma função calcular_imposto() que calcula um imposto de 5% e retorna a quem pediu...
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 #aqui é o uso... aqui é o imposto a calcular... e exibir na tela
 preco = 299
 imposto = calcular_imposto(preco)
 print(imposto)
+
+print(preco)
+
+#agora calcular imposto a alíquota agora é 7%
+
+valores = [1.99, 24.50, 70.27, 1515.5]
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)} ")
+
 
   
   
