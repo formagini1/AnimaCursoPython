622d3202041e96cf6db048ec6c76f6f36bfbbc82
Ryan Augusto <90337265+formagini1@users.noreply.github.com>
2022-11-29 12:31:47 +0000

    atividade para conclusão do curso python.

---
+++ atividade.py
@@ -0,0 +1,24 @@
+nome  =  input ( "Digite seu nome: " )
+idade  =  int ( input ( "Digite sua idade: " ))
+
+
+print ( f"Boa noite, seu nome é { nome } " )
+print ( f"Sua idade é { idade } " )
+
+if idade  >=  18 :
+  print ( "voce é maior de idade, ótimo! já pode tirar sua carteira de carro ou moto" )
+else :
+  print ( f"voce não é maior de idade ainda { nome } ..." )
+  
+
+
+# Outro projeto que estou olhando
+  
+nome =  input ( 'Qual é o seu nome?' )
+print ( 'Olá' , nome , '!' ',Seja bem vindo!' )
+dia =  input ( 'dia =' )
+mes =  input ( 'Mes =' )
+ano =  input ( 'Ano =' )
+print ( 'Voce nasceu no' , dia , 'do' , mes , 'do' , ano , '.' )
+correto    =  input ( 'correto?' )
+print ( 'Obrigado' , nome , 'pela participação!' )
